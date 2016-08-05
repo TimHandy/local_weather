e2be3473629b4e4c6ca376438d0cb860a8c4b192
Tim Handy <tjhandy@gmail.com>
2016-08-05 07:39:14 +0100

    update html, js, css in Weather app

--- default.js
+++ default.js
@@ -1,112 +1,81 @@
+var weatherAPIKey = '&APPID=a66f4e53741f129977e971853722ab80';
+
+//Example API string: http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80&lat=51.5276880&lon=-2.5410160&units=metric
+function getWeatherQueryString(lat, lon){
+	return "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?" + weatherAPIKey + "&lat=" + lat + "&lon=" + lon + "&units=metric"; // the full API string with lat and lon, and metric units. crossorigin is to combat the http limitation of openweathermap. The page on codepen must load with https for the geolocation function to work, but openweather only allows http.
+}
+
+
+function handleWeatherResponse(json) {  // take the data from the json call and output to webpage
+	var weatherData = json;		// need to specify var so that it's local to the function.
+	console.log(weatherData);
+	//console.log("Weather Data Grabbed");
+	$("h1").text("Your Local Weather for " +weatherData.name);  // by putting this here and calling sendRequest below, on page load it displays the data straight away!
+		$("#weather-desc").text(weatherData.weather[0].description);  // pass these directly in, rather than creating variables for them first.
+		var celcius = weatherData.main.temp
+		var Fahrenheit = celcius * 1.8 + 32;
+		$("#tempC").text(Math.floor(celcius) + "°C");
+		$("#tempF").text(Math.floor(Fahrenheit) + "°F");
+		$("#wind-speed").text(weatherData.wind.speed + " mph");
+		var weatherIconUrl = "<img src=https://crossorigin.me/http://openweathermap.org/img/w/";
+		var weatherIconPng = ".png>"
+		$("#weather-icon").html(weatherIconUrl + weatherData.weather[0].icon + weatherIconPng);
+}
+
+
+// Get latitude and longitude, build weather query string, send weather query request
+function sendRequest() {
+	if (navigator.geolocation) { // IDEA: if can't find, give an error message? give an option to search for location?
+		navigator.geolocation.getCurrentPosition(function(position){
+
+			var weatherQueryString = getWeatherQueryString(position.coords.latitude.toFixed(2), position.coords.longitude.toFixed(2)); // extracted the query string creation to getWeatherQueryString function.
+			console.log(weatherQueryString);
+
+			$.getJSON(weatherQueryString, handleWeatherResponse); // data response is passed to handleWeatherResponse
+		});
+	}
+}
+
+
+
+// function toggleTemp() {
+//   var celsius = weatherData.main.temp;    // won't work as haven't got access to weatherData
+//   var Fahren = celcius * 1.8 + 32;    // convert C to F
+//   if (code on page shows degrees C) {
+//      convert to degrees f;
+//      replace on page with Fahren;
+//    } else {
+//      replace on page with celcius;
+//    }
+// }
+
 
 // User Story: I can see the weather in my current location.
+sendRequest();  // call in body so that it runs at page load to display the weather straight away
 
-//Example API call: http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80&lat=51.5276880&lon=-2.5410160
-
-var lat = '';
-var lon = '';
-var weatherAPIKey = '&APPID=a66f4e53741f129977e971853722ab80';
-var weatherQueryString = ''
-var weatherData = '';
-// Get latitude and longitude... and now weather string... and now weather query!
-if (navigator.geolocation) { 	// TODO: or if can't find, give an error message? give an option to search
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    console.log(lat);
-    console.log(lon);
-    var weatherQueryString = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?" + weatherAPIKey + "&lat=" + lat + "&lon=" + lon + "&units=metric"; // the full API string with lat and lon
-    console.log(weatherQueryString);
-
-    $.getJSON(weatherQueryString, function(json) {
-      //var weatherData = JSON.stringify(json);
-			weatherData = json;
-      console.log(weatherData);
-      console.log("Weather Data Grabbed");
-    });
-  });
-};
 
 
 // Output raw weather data to test
 $(document).ready(function() {
-  $("#get-weather").on("click", function() {		// FIXME: can I make this work on page load instead of with a button?
-    //getWeatherData();  // want to be able to call the getWeatherData function... haven't got it named since smashing it into the lat and lon funtion! - need to name this function on line 18? not sure how?
-	 $("#location").text(weatherData.name);
-    $("#weather-desc").text(weatherData.weather[0].description);
-	 $("#temp").text(weatherData.main.temp + " degrees C");
-	  $("#wind-speed").text(weatherData.wind.speed + " mph");
-    // alert("hey");
-  });
+	$("#get-weather").on("click", sendRequest );  // read up on difference between passing the reference to the function and passing the result of the function. Notice differnce between with and without parens at end of function!
+
+// User Story: I can push a button to toggle between Fahrenheit and Celsius.
+ $("#toggle-c-f").on("click", function() {
+	$(".toggle").toggle();
+ });
 });
 
 
 
-
-
-//$("#demo").txt(data);
-
-
- // Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
-
-// store the results
-// parse the results to variables
-// display the variables on the page
+// TODO: User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
 
 
 
-
-
-
-
-//var temp = '';
-//var windspeed = '';
-
-// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
-
-// User Story: I can push a button to toggle between Fahrenheit and Celsius.
-
-// We recommend using the Open Weather API. This will require creating a free API key. Normally you want to avoid exposing API keys on CodePen, but we haven't been able to find a keyless API for weather.
-
-
-
-
-
-
-
-
-
-
-// getLatLon();		// assign to a var here?
-
-// User Story: I can see the weather in my current location.
-
-// call the weather API
-//
+ // TODO: re weather api... Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
 
 
 // The getJSON in getLatLon is async, so had to use a callback to wait for the data before console.logging the info. Might need this later if other functions don't return what's expected.
 
-
-//$("#demo").txt(data);
-
-// Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
-
-// store the results
-// parse the results to variables
-// display the variables on the page
-
-//Example API call:
-// http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80&lat=51.5276880&lon=-2.5410160
-
-//var temp = '';
-//var windspeed = '';
-
-// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
-
-// User Story: I can push a button to toggle between Fahrenheit and Celsius.
-
-// We recommend using the Open Weather API. This will require creating a free API key. Normally you want to avoid exposing API keys on CodePen, but we haven't been able to find a keyless API for weather.
 
 // $(document).ready(function() {
 // 	$("#get-weather").on("click", function() {
@@ -126,128 +95,4 @@ function getLatLong(callback) { // function definition
     lon = location.split(",")[1];
     callback(lat, lon);   // this is some advanced next-level shit I don't understand
   });
-
-
-
-// getLatLon();		// assign to a var here?
-
-// User Story: I can see the weather in my current location.
-
-// call the weather API
-//
-
-
-// The getJSON in getLatLon is async, so had to use a callback to wait for the data before console.logging the info. Might need this later if other functions don't return what's expected.
-
-
-//$("#demo").txt(data);
-
-// Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
-
-// store the results
-// parse the results to variables
-// display the variables on the page
-
-//Example API call:
-// http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80&lat=51.5276880&lon=-2.5410160
-
-//var temp = '';
-//var windspeed = '';
-
-// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
-
-// User Story: I can push a button to toggle between Fahrenheit and Celsius.
-
-// We recommend using the Open Weather API. This will require creating a free API key. Normally you want to avoid exposing API keys on CodePen, but we haven't been able to find a keyless API for weather.
-
-// $(document).ready(function() {
-// 	$("#get-weather").on("click", function() {
-// 		//  getLatLon(function(data) {
-// 		$("#location").text(lat);
-// 		//   });
-// 	});
-// });
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-function getLatLong(callback) { // function definition
-  $.getJSON('http://ipinfo.io', function(data) {
-    var location = data.loc;
-    lat = location.split(",")[0];
-    lon = location.split(",")[1];
-    callback(lat, lon);   // this is some advanced next-level shit I don't understand
-  });
-}
-
-$(document).ready(function() {
-  $("#get-weather").on("click", function() {  // target the get-weather id and on clicking it, call the next function...
-    getLatLong(function(lat, lon) {
-      console.log(lat);
-      $("#location").text(lat);
-    });
-  });
-});
-*/
-
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     lat = position.coords.latitude; // 51.5191334
-//     lon = position.coords.longitude; // -2.5400082999999998
-//    apiKey = "http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80";
-//     weatherQuery = apiKey + "&lat=" + lat + "&lon=" + lon;   // the full API string for lat and lon
-//   });
-// }
-
-
-
-
-
-
-
-// function getLatLon() {
-// 	 $.getJSON('http://ipinfo.io', function(data) {
-// 		var location = data.loc;
-// 		lat = location.split(",")[0];
-// 		lon = location.split(",")[1];
-// 		// getWeatherData();  // this is in the callback of the getJson command? Isn't this doing too much?
-// 	});
-// 	console.log("Lat: " + lat + "Lon: " + lon)
-// }
-
-// }
-//
-// $(document).ready(function() {
-//   $("#get-weather").on("click", function() {  // target the get-weather id and on clicking it, call the next function...
-//     getLatLong(function(lat, lon) {
-//       console.log(lat);
-//       $("#location").text(lat);
-//     });
-//   });
-// });
-
-
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     lat = position.coords.latitude; // 51.5191334
-//     lon = position.coords.longitude; // -2.5400082999999998
-//    apiKey = "http://api.openweathermap.org/data/2.5/weather?&APPID=a66f4e53741f129977e971853722ab80";
-//     weatherQuery = apiKey + "&lat=" + lat + "&lon=" + lon;   // the full API string for lat and lon
-//   });
-// }
-
-
-
-
-
-
-
-// function getLatLon() {
-// 	 $.getJSON('http://ipinfo.io', function(data) {
-// 		var location = data.loc;
-// 		lat = location.split(",")[0];
-// 		lon = location.split(",")[1];
-// 		// getWeatherData();  // this is in the callback of the getJson command? Isn't this doing too much?
-// 	});
-// 	console.log("Lat: " + lat + "Lon: " + lon)
-// }
+ */
